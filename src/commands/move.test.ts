52b78862611944c57a5563a284c7ceb16e45844a
Song <songsongha@gmail.com>
2024-08-10 07:39:19 -0600

    Remove comments

--- src/commands/move.test.ts
+++ src/commands/move.test.ts
@@ -25,25 +25,3 @@ describe('function: move', () => {
         expect(logSpy.mock.calls[0][1]).toBe('Invalid move')
     })
 })
-
-// export function move(distance: number, position: Coordinate, direction: Direction): Coordinate {
-//     let newPosition: Coordinate = [...position]
-//     switch (direction) {
-//         case 'NORTH':
-//             newPosition[1] = newPosition[1] + distance
-//             break
-//         case 'EAST':
-//             newPosition[0] = newPosition[0] + distance
-//             break
-//         case 'SOUTH':
-//             newPosition[1] = newPosition[1] - distance
-//             break
-//         case 'WEST':
-//             newPosition[0] = newPosition[0] - distance
-//     }
-//     if (newPosition.filter((element) => element < 0 || element > 100).length) {
-//         console.log('\x1b[31m%s\x1b[0m', 'Invalid move')
-//         return position
-//     }
-//     return newPosition
-// }
