7fadbe10e1d2d537d7f3e462111cf56a7f1ca801
Song <songsongha@gmail.com>
2024-08-19 08:58:53 -0600

    Add mocks for commands and additional unit tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -9,7 +9,7 @@ import readline from 'readline'
 // Mock readline module
 const mockClose = jest.fn()
 jest.mock('readline', () => {
-    const responseQueue = ['LAND', 'MOVE 10', 'LEFT', 'REPORT', 'MOVE 5']
+    const responseQueue = ['LAND', 'LAND', 'LAND', 'LEFT', 'LEFT', 'REPORT', 'MOVE 5']
 
     const mockQuestion = jest.fn((query: string, callback: (input: string) => void) => {
         const response = responseQueue.shift() || 'LAND'
@@ -25,18 +25,58 @@ jest.mock('readline', () => {
     }
 })
 
-const mockGetRandomNumber = jest.fn().mockReturnValue(22)
+const mockGetRandomNumber = jest.fn().mockName('mockGetRandomNumber').mockReturnValue(22)
+const mockIsSamePosition = jest.fn().mockName('mockIsSamePosition').mockReturnValue(true)
 jest.mock('./utils/helpers', () => ({
-    ...jest.requireActual('./utils/helpers'),
-    getRandomNumber: () => mockGetRandomNumber()
+    // ...jest.requireActual('./utils/helpers'),
+    getRandomNumber: () => mockGetRandomNumber(),
+    isSamePosition: () => mockIsSamePosition()
+}))
+
+const mockMove = jest.fn().mockName('mockMove')
+jest.mock('./commands/move', () => ({
+    move: () => mockMove()
+}))
+
+const mockReportLocation = jest.fn().mockName('mockReportLocation')
+jest.mock('./commands/report', () => ({
+    reportLocation: () => mockReportLocation()
+}))
+
+const mockRotate = jest.fn().mockName('mockRotate')
+jest.mock('./commands/rotate', () => ({
+    rotateDirection: () => mockRotate()
+}))
+const mockListValidCommands = jest.fn().mockName('mockListValidCommands')
+jest.mock('./commands/listValidCommands', () => ({
+    listValidCommands: () => mockListValidCommands()
 }))
 
 describe('findObiWan()', () => {
+    beforeEach(() => {
+        jest.clearAllMocks()
+    })
     it('should call getRandomNumber if the LAND command is input by the user', async () => {
+        console.log('TEST1')
         await findObiWan()
 
-        expect(mockGetRandomNumber).toHaveBeenCalled()
+        expect(mockGetRandomNumber).toHaveBeenCalledTimes(4)
+        expect(mockReportLocation).toHaveBeenCalledTimes(1)
+        expect(mockListValidCommands).toHaveBeenCalledTimes(1)
+        expect(mockMove).not.toHaveBeenCalled()
+        expect(mockRotate).not.toHaveBeenCalled()
         expect(mockClose).toHaveBeenCalled()
     })
-    it('should call rotateDirection function if the LEFT command is input by the user', async () => {})
+    it('should call rotateDirection function if the LEFT command is input by the user', async () => {
+        console.log('TEST 2')
+        mockIsSamePosition.mockReturnValueOnce(false).mockReturnValueOnce(true)
+        await findObiWan()
+
+        expect(mockGetRandomNumber).toHaveBeenCalledTimes(4)
+        expect(mockReportLocation).toHaveBeenCalledTimes(1)
+        expect(mockListValidCommands).toHaveBeenCalledTimes(1)
+        expect(mockMove).not.toHaveBeenCalled()
+        expect(mockRotate).toHaveBeenCalledTimes(1)
+        expect(mockClose).toHaveBeenCalled()
+    })
 })
